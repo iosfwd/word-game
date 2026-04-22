1fcb2727875f49168eb96ff0224065fe02be152d
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-04-22 15:47:38 +0900

    check game status in board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,7 +1,25 @@
-const Board = () => {
+import Row from './row';
+import type { EvaluatedGuess, GameStatus } from '../types';
+
+type Props = {
+  guesses: EvaluatedGuess[];
+  currentGuess: string;
+  gameStatus: GameStatus;
+}
+
+const Board = ({ guesses, currentGuess, gameStatus }: Props) => {
   return (
-    <>
-    </>
+    <div>
+      {Array.from({ length: 6}).map((_, i) => {
+	if (i < guesses.length) {
+	  return <Row key={i} status={'committed'} guess={guesses[i]} />;
+	} else if (i === guesses.length && gameStatus === 'ongoing') {
+	  return <Row key={i} status={'active'} letters={currentGuess} />;
+	} else {
+	  return <Row key={i} status={'empty'} />;
+	}
+      })}
+    </div>
   );
 }
 
