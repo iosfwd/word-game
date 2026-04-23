dbdd8a3dd8ec67e5c2d53d48bc971b14f1244213
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-04-23 11:49:52 +0900

    add base interactivity to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Board from './components/board';
 import { evaluateGuess, isValidWord, getRandomWord, getGameStatus } from './utils';
 import type { EvaluatedGuess, GameStatus } from './types';
 import styles from './app.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const App = () => {
   const [solution] = useState<string>(getRandomWord());
@@ -10,29 +10,45 @@ const App = () => {
   const [currentGuess, setCurrentGuess] = useState<string>('');
   const [gameStatus, setGameStatus] = useState<GameStatus>('ongoing');
 
-  const handleLetter = (letter: string) => {
+  const handleLetter = useCallback((letter: string) => {
     if (currentGuess.length < 5 && gameStatus === 'ongoing') {
       setCurrentGuess(prev => prev + letter);
     }
-  }
+  }, [currentGuess, gameStatus]);
 
-  const handleBackspace = () => {
+  const handleBackspace = useCallback(() => {
     if (gameStatus === 'ongoing') {
       setCurrentGuess(prev => prev.slice(0, -1))
     }
-  }
+  }, [currentGuess, gameStatus]);
 
-  const handleEnter = () => {
+  const handleEnter = useCallback(() => {
     if (currentGuess.length === 5 && gameStatus === 'ongoing') {
       // TODO: handle invalid word
       if (isValidWord(currentGuess)) {
 	const retval = evaluateGuess(currentGuess, solution);
-	setGuesses(prev => prev.concat(retval));
 	const newGuesses = [...guesses, retval];
+	setGuesses(newGuesses);
 	setGameStatus(getGameStatus(newGuesses));
+	setCurrentGuess('');
       }
     }
-  }
+  }, [currentGuess, guesses, gameStatus]);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleEnter();
+    } else if (e.key === 'Backspace') {
+      handleBackspace();
+    } else if (/^[a-zA-Z]$/.test(e.key)) {
+      handleLetter(e.key.toLowerCase());
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [handleKeyDown])
 
   return (
     <div className={styles.app}>
